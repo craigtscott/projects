a4b8f4239551d32746150f7c273ac5a2ec62c3bf
Craig Scott <craigtscottjr@gmail.com>
2016-11-25 08:28:27 -0800

    Adds chess folder w2d1 and w2d4

--- w1d3/work/fib.rb
+++ w1d3/work/fib.rb
@@ -3,3 +3,14 @@ def fibonacci(num)
 
   fibonacci(num-1) + fibonacci(num-2)
 end
+require 'byebug'
+def fib_list(num)
+  return [0] if num == 0
+  return [1] if num <= 1
+
+# debugger
+last_fib = fib_list(num - 1)
+two_ago = fib_list(num - 2 )
+next_fib = last_fib.last + two_ago.last
+return last_fib << next_fib
+end
